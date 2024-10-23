e95ade9b58c824aaeba8ca7c222841ba0c7d4942
h.habka <h.habka@staff.ososs.com>
2024-10-23 16:13:24 +0400

    fix lazy loading

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { onlyMeImages } from "./images/onlyMe"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import "react-lazy-load-image-component/src/effects/blur.css"
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 const randomizeFunction = () => {
   const randInt = parseInt(Math.random() * 100)
@@ -15,48 +16,34 @@ const randomizedArray = randomizeArray(onlyMeImages)
 
 function App() {
   return (
-    <div>
-      {randomizedArray.map((item) => {
-        return (
-          <LazyLoadImage
-            effect="blur"
-            wrapperProps={{
-              style: { transitionDelay: "1s" },
-            }}
-            src={item}
-            key={item}
-            alt={item}
-          />
-        )
-      })}
+    <div className="">
+      <div>
+        <p>Hi </p>
+      </div>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1024: 4, 1440: 5 }}
+      >
+        <Masonry columnsCount={4} gutter="10px">
+          {randomizedArray.map((item) => {
+            return (
+              <LazyLoadImage
+                effect="blur"
+                wrapperProps={{
+                  style: { transitionDelay: "1s" },
+                }}
+                src={item}
+                key={item}
+                alt={item}
+                width={700}
+                height={800}
+                className="rounded-lg"
+              />
+            )
+          })}
+        </Masonry>
+      </ResponsiveMasonry>
     </div>
   )
-  // return (
-  //   <div className="">
-  //     <div>
-  //       <p>Hi </p>
-  //     </div>
-  //     <ResponsiveMasonry
-  //       columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1024: 4, 1440: 5 }}
-  //     >
-  //       <Masonry columnsCount={4} gutter="10px">
-  //         {randomizedArray.map((item) => {
-  //           return (
-  //             <LazyLoadImage
-  //               effect="blur"
-  //               wrapperProps={{
-  //                 style: { transitionDelay: "1s" },
-  //               }}
-  //               src={item}
-  //               key={item}
-  //               alt={item}
-  //             />
-  //           )
-  //         })}
-  //       </Masonry>
-  //     </ResponsiveMasonry>
-  //   </div>
-  // )
 }
 
 export default App
